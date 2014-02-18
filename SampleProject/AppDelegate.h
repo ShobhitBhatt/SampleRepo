e13807ddc4e314680e1ecce840ed3aba6306c295
ShobhitBhatt <shobhit.bhatt1987@gmail.com>
2014-02-18 16:15:42 +0530

    Initial commit

---
+++ SampleProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SampleProject
+//
+//  Created by Shobhit Bhatt on 18/02/14.
+//  Copyright (c) 2014 Shobhit.bhatt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
