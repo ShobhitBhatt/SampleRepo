3123dc2f2af1a903d14b8ae7dfb6704ee0ae5474
ShobhitBhatt <shobhit.bhatt@infostretch.com>
2014-03-11 17:14:48 +0530

    [ISC] Epic: WPHRT-61 >> Story: WPHRT-62 >> Task: WPHRT-65; This is a test commit message.

--- SampleProject/ViewController.h
+++ SampleProject/ViewController.h
@@ -3,7 +3,7 @@
 //  SampleProject
 //
 //  Created by Shobhit Bhatt on 18/02/14.
-//  Copyright (c) 2014 Shobhit.bhatt. All rights reserved.
+//  Copyright (c) 2014 Shobhit.bhatt. All rights reserved. 
 //
 
 #import <UIKit/UIKit.h>   
